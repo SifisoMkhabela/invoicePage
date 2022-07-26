b853b736cbaf00132b2d7fdd6b2f67005d665699
siphephelon <107863625+siphephelon@users.noreply.github.com>
2022-07-26 14:46:49 +0200

    up

--- src/main/java/com/dev/springboot/view/InvoiceDataPdfExport.java
+++ src/main/java/com/dev/springboot/view/InvoiceDataPdfExport.java
@@ -49,20 +49,40 @@ public class InvoiceDataPdfExport extends AbstractPdfView {
         //total
         double totalAmount = 00;
         //download PDF with a given filename
-        response.addHeader("Content-Disposition", "attachment;filename=Invoices.pdf");
+        response.addHeader("Content-Disposition", "attachment;filename=Invoice.pdf");
+
+        //adding image to pdf
+        String imagePath = "images\\logo.png";
+        //creating image data object
+        ImageData data = ImageDataFactory.create(imagePath);
+        //instatiating image class
+        Image img = new Image(data);
+        img.scaleToFit(200, 200); //imAGE RESCALLLING
+        //adding image to document
+        document.add(img);
 
         //read data from controller
         List<Invoice> list = (List<Invoice>) model.get("list");
 
-        //create element
-        Font titleFont = new Font(Font.TIMES_ROMAN, 24, Font.BOLD, Color.blue);
-        Paragraph customer = new Paragraph("Customer Details", titleFont);
-        Paragraph title = new Paragraph("INVOICE ITEMS", titleFont);
-        title.setAlignment(Element.ALIGN_CENTER);
-        title.setSpacingBefore(20.0f);
-        title.setSpacingAfter(25.0f);
-        //add to document title
-        document.add(title);
+        //Setting font
+        Font cartFont = new Font(Font.TIMES_ROMAN, 24, Font.BOLD, Color.blue);
+
+        //Company Profile
+        Paragraph companyProfile = new Paragraph("Lorem Ipsum\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat");
+        document.add(companyProfile);
+
+        //Adding Customer Details
+        Paragraph customer = new Paragraph("Customer Details", cartFont);
+        Paragraph customerDetails = new Paragraph("PepeBoii \nHouse 1220\nHappy Drive\nAncient City");
+        document.add(customer);
+        document.add(customerDetails);
+
+        Paragraph cart = new Paragraph("INVOICE ITEMS", cartFont);
+        cart.setAlignment(Element.ALIGN_CENTER);
+        cart.setSpacingBefore(20.0f);
+        cart.setSpacingAfter(25.0f);
+        //add to document cart
+        document.add(cart);
 
         //header for invoice Items
         Font tableHead = new Font(Font.TIMES_ROMAN, 12, Font.BOLD, Color.blue);
@@ -94,10 +114,18 @@ public class InvoiceDataPdfExport extends AbstractPdfView {
 
         System.out.println("R" + f.format(totalAmount));
 
+
+
         //parse data to pdf document
         document.add(totalTable);
 
+        //Banking Details
+         Paragraph bankingDetails = new Paragraph(" Duis aute irure dolor\nin reprehenderit in\nvoluptate velit esse cillum dolore eu\nfugiat nulla pariatur.")
+         document.add(bankingDetails);
 
+        //Terms and Conditions
+         Paragraph termsAndConditions = new Paragraph("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
+         document.add(termsAndConditions);
 
         //
     }
